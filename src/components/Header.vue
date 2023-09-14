f6373a1386ed57f59a1cd39e2571c0c80f1c4019
Sye-5pace <owusilasyp00@gmail.com>
2023-09-14 13:57:04 +0100

    Header cart card created.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"> 
-
+    import { ref,computed } from 'vue'
+    
+    const clicked = ref<boolean>(false)
 </script>
 
 <template>
@@ -15,10 +17,18 @@
             </div>
         </div>
         <div class="flex gap-[1.5rem] items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer stroke-palesky hover:stroke-shark">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cursor-pointer stroke-palesky hover:stroke-shark" @click="clicked = !clicked">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
             <img src="../assets/image-avatar.png" class="w-[3.125rem] cursor-pointer hover:border-2 hover:border-pumpkin hover:rounded-full"/>
+        </div>
+        <div class="bg-[#fff] absolute w-[22.5rem] h-[16rem] grid grid-rows-[25%_75%] inset-0 left-[57%] top-[9%] rounded-[0.625rem] shadow-lg bg-[rgba(29, 32, 38, 0.50)]" :class="clicked ? 'block' : 'hidden'" >
+            <div class="p-4 border-b border-catskwhite">
+                <h3 class="font-bold">Cart</h3>
+            </div>
+            <div class="flex items-center justify-center">
+                <h3 class="text-[1rem] font-bold text-palesky">Your card is empty.</h3>
+            </div>
         </div>
     </header>
 </template>
