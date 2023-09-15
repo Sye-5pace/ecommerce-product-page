ea9caf68ef1b88326a089b9db37b5081b2e2bc0d
Sye-5pace <owusilasyp00@gmail.com>
2023-09-15 15:57:46 +0100

    Product component UI updated

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -8,7 +8,7 @@
             <div>
                 <img src="../assets/image-product-1-thumbnail.jpg" class="cursor-pointer w-[27.8125rem] h-[27.8125rem] rounded-[0.9375rem]" alt="">
             </div>
-            <div class="flex gap-4 children:cursor-pointer">
+            <div class="flex gap-7 children:cursor-pointer">
                 <img src="../assets/image-product-1-thumbnail.jpg" alt="" class="w-[5.5rem] rounded-[0.625rem] hover:border-2 hover:border-pumpkin">
                 <img src="../assets/image-product-2-thumbnail.jpg" alt="" class="w-[5.5rem] rounded-[0.625rem] hover:border-2 hover:border-pumpkin">
                 <img src="../assets/image-product-3-thumbnail.jpg" alt="" class="w-[5.5rem] rounded-[0.625rem] hover:border-2 hover:border-pumpkin">
